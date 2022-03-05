b7d4cc96daac22db1249508b23e0222e19f2c11a
Teodor Proca <procateodor@Bytexs-MacBook-Pro-4.local>
2022-03-05 12:36:29 +0200

    Init project

---
+++ src/Header/Header.js
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Home</Link>
+      <Link to="/products">Products</Link>
+      <Link to="/categories">Categories</Link>
+      <Link to="/cart">Cart</Link>
+      <Link to="/faq">FaQ</Link>
+      <Link to="/contact">Contact</Link>
+    </header>
+  );
+}
+
+export default Header;
